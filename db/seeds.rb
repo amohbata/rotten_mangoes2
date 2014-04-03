42a7a02bc152e2b09632fe4eb453952659145558
amohbata <amohbata@gmail.com>
2014-04-03 21:08:47 +0000

    user admin functionality

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(
+	email:"boyshit@shit.com",
+	firstname:"boy",
+	lastname:"shit",
+	password:"1234567
+	",
+	password_confirmation:"1234567",
+	admin:true
+)
+
+User.create(
+	email:"boywonder@shit.com",
+	firstname:"boy",
+	lastname:"wonder",
+	password:"1234567",
+	password_confirmation:"1234567",
+	admin:false
+)
+
+User.create(
+	email:"cheetah@shit.com",
+	firstname:"boy",
+	lastname:"wonder",
+	password:"1234567",
+	password_confirmation:"1234567",
+	admin:false
+)
